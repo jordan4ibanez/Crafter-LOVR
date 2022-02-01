d0c82222acb36dcc78c0aec8226b40e06435c06f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-31 20:52:48 -0500

    Add in skeleton of geometry tutorial to reimplement

--- game/main.lua
+++ game/main.lua
@@ -1,6 +1,7 @@
 require("dump")
 require("chunkMeshBuilder")
 require("map")
+local fragmentShader = require("shader")
 
 -- lua locals
 local
@@ -8,6 +9,30 @@ lovr
 = 
 lovr
 
+local function makeShader(prefix)
+    return lovr.graphics.newShader(prefix .. [[
+  out vec3 lightDirection;
+  out vec3 normalDirection;
+  out vec3 vertexPosition;
+  
+  vec3 lightPosition = vec3(10., 10., 3.);
+  
+  vec4 position(mat4 projection, mat4 transform, vec4 _vertex) {
+    vec4 vertex = preTransform(_vertex);
+  
+    vec4 vVertex = transform * vertex;
+    vec4 vLight = lovrView * vec4(lightPosition, 1.);
+  
+    lightDirection = normalize(vec3(vLight - vVertex));
+    normalDirection = normalize(lovrNormalMatrix * lovrNormal);
+    vertexPosition = vVertex.xyz;
+  
+    return projection * transform * vertex;
+  }
+  ]], fragmentShader)
+  end
+
+local mesh1Program = makeShader("vec4 preTransform(vec4 v) { return v; }")
 
 local chunkMesh
 
@@ -24,5 +49,13 @@ end
 
 
 function lovr.draw()
-    local delta = lovr.timer.getDelta()
+
+    lovr.graphics.setShader(mesh1Program)
+
+    lovr.graphics.push() -- White triangle
+    lovr.graphics.setColor(1,1,1)
+    lovr.graphics.translate(0, 0, -2)
+    chunkMesh:draw(0,0,0)
+    lovr.graphics.pop()
+
 end
