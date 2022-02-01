4c290dc00086b86cb720534640fc184cc2ef8cb7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-31 19:36:19 -0500

    Implement extreme data oriented approach

--- game/map.lua
+++ game/map.lua
@@ -7,17 +7,77 @@ lovr
 math.randomseed(os.time())
 
 -- Holds all chunk data
-local __chunk_table = {}
 
---Simple keygenner
+local __chunk_key       = {}
+local __chunk_dimension = {}
+local __chunk_blocks    = {}
+local __chunk_rotations = {}
+local __chunk_light     = {}
+local __chunk_heightMap = {}
+
+-- Simple keygenner
 local function keyGen(x,z)
     return tostring(x) .. " " .. tostring(z)
 end
 
--- Internal adder for chunk creation
-local function __create_chunk(key)
+-- Simple 3D to 1D converter
+local function posToIndex(x, y, z)
+    return (z * 2048) + (y * 16) + x
+end
 
-    __chunk_table[key] = {
+-- Internal adder for chunk creation
+local function __create_chunk(x, z, dimension)
+
+    local index = #__chunk_key
+
+    __chunk_key[keyGen(x,z)] = index
+
+    __chunk_dimension[index] = dimension
+
+    -- Biome generator init goes here
+    __chunk_blocks[index] = {}
+    __chunk_rotations[index] = {}
+    __chunk_light[index] = {}
+
+    --[[
+        Blank chunk creation.
+        A biome generator function will be created in the future maybe
+    
+        This is still 2D and needs to have more math backing a pure 1D implementation
+    ]]--
+    for i = 1, 32768 do
+        __chunk_blocks[index][i]    = 1
+        __chunk_rotations[index][i] = 0
+        __chunk_light[index][i]     = 0
+    end
+
+    __chunk_heightMap[index] = {}
+
+     -- Heightmap data
+     for i = 1,256 do
+        __chunk_heightMap[index][i] = 0
+    end
+
+    return(index)
+end
+
+-- External oneline creator which returns the index reference to the chunk
+function NewChunk(x, z, dimension)
+    return __create_chunk(x,z, dimension)
+end
+
+-- Simple testing of getting lua chunk object data from tables
+-- Returns as object or nil
+function GetChunk(x, z)
+
+    local key = keyGen(x,z)
+    local chunkIndex = __chunk_key[key]
+
+    if chunkIndex == nil then
+        return nil
+    end
+
+    local chunkContainer = {
         dimension = nil,
         blocks = {},
         rotations = {},
@@ -25,45 +85,35 @@ local function __create_chunk(key)
         heightMap = {},
     }
 
-    return(__chunk_table[key])
+    chunkContainer.dimension = __chunk_dimension[chunkIndex]
 
-end
-
--- External oneline creator which returns the pointer reference to the chunk
-function NewChunk(x,z)
-
-    local key = keyGen(x,z)
-
-    local chunkPointer = __create_chunk(key)
-
-    -- All chunks are in the "overworld" for now
-    chunkPointer.dimension = "overworld"
-
-    --[[
-    Blank chunk creation.
-    A biome generator function will be created in the future maybe
-    ]]--
     for i = 1, 32768 do
-        chunkPointer.blocks[i] = 1
-        chunkPointer.rotations[i] = 0
-        chunkPointer.light[i] = 0
+        chunkContainer.blocks[i]    = __chunk_blocks[chunkIndex][i]
+        chunkContainer.rotations[i] = __chunk_rotations[chunkIndex][i]
+        chunkContainer.light[i]     = __chunk_light[chunkIndex][i]
     end
 
-    -- Heightmap data
     for i = 1,256 do
-        chunkPointer.heightMap = 0
+        chunkContainer.heightMap[i] = __chunk_heightMap[chunkIndex][i]
     end
 
-    return chunkPointer
-
-end
-
--- Simple testing of getting lua chunk object data from table
-function GetChunk(x,z)
-    return __chunk_table[keyGen(x,z)]
+    return chunkContainer
 end
 
 -- Simple testing of removing chunks from container
-function DeleteChunk(x,z)
-    __chunk_table[keyGen(x,z)] = nil
+function DeleteChunk(x, z)
+
+    local key = keyGen(x,z)
+    local chunkIndex = __chunk_key[key]
+
+    if chunkIndex == nil then
+        return false
+    end
+
+    __chunk_dimension[chunkIndex] = nil
+    __chunk_blocks[chunkIndex]    = nil
+    __chunk_rotations[chunkIndex] = nil
+    __chunk_light[chunkIndex]     = nil
+    __chunk_heightMap[chunkIndex] = nil
+    __chunk_key[key] = nil
 end
