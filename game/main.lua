3eea5a6f37e76fde33487021165b8571f2ddd808
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-31 19:41:12 -0500

    Add debug

--- game/main.lua
+++ game/main.lua
@@ -11,6 +11,7 @@ lovr
 function lovr.load(args)
     for x = -100, 100 do
         for z = -100, 100 do
+            print("Generating: " .. x .. " " .. z)
             NewChunk(x,z,"overworld")
         end
     end
