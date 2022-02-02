4a61db9fb13ee57ebdd50736d31ee3346b3b669a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-01 20:11:30 -0500

    Update chunkMeshBuilder.lua

--- game/chunkMeshBuilder.lua
+++ game/chunkMeshBuilder.lua
@@ -2,8 +2,21 @@
 function BuildChunkMesh(chunk)
     local newChunkMesh
     -- This mesh is a single triangle
-    newChunkMesh = lovr.graphics.newMesh({{ 'lovrPosition', 'float', 3 }, { 'lovrNormal', 'float', 3 }}, 3, 'triangles')
-    newChunkMesh:setVertices({{0,0,0, 0,0,1}, {1,0,0, 0,0,1}, {0,1,0, 0,0,1}})
+    newChunkMesh = lovr.graphics.newMesh(
+        {
+            { 'lovrPosition', 'float', 3 },
+            { 'lovrNormal', 'float', 3 }
+        },
+        3,
+        'triangles'
+    )
+    newChunkMesh:setVertices(
+        {
+            {0,0,0, 0,0,1},
+            {1,0,0, 0,0,1},
+            {0,1,0, 0,0,1},
+        }
+    )
     
     return newChunkMesh
 end
