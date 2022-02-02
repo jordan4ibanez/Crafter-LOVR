becf05bd390d810de130debd146a24f54be80d05
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-01 20:04:43 -0500

    Update camera.lua

--- game/camera.lua
+++ game/camera.lua
@@ -1,7 +1,13 @@
+require("lovr-libraries.lovr-mouse")
+
 local x = 0
 local y = 0
 local z = 0
 
 function GetCameraPos()
     return {x=x, y=y, z=z}
+end
+
+function MoveCamera()
+   print("test")
 end
