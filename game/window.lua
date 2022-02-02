64b636c976374fc4ab3382b32ffad91a4811332f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-01 20:46:51 -0500

    Update window.lua

--- game/window.lua
+++ game/window.lua
@@ -1,2 +1,23 @@
-require("lovr-libraries.lovr-window")
+local window = require("lovr-libraries.lovr-window")
 
+local title
+
+function InitializeWindow()
+    title = "Crafter " .. GetVersionName()
+
+    -- sets window opacity, resolution and title
+    local width, height = window.getDisplayDimensions(1)
+
+    window.setMode(width / 2, height / 2, {opacity = 1, fullscreentype = "desktop", resizable = true})
+
+    window.setTitle(title)
+
+    local width, height, mode = window.getMode()
+
+    print(width .. " " .. height .. " " .. dump(mode))
+
+end
+
+function lovr.resize()
+    lovr.graphics.reset()
+end
