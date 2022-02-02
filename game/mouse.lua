1d843436077635a423e654ab76061b6645f2aecb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-01 21:26:59 -0500

    Update mouse.lua

--- game/mouse.lua
+++ game/mouse.lua
@@ -1,1 +1,14 @@
-print("I'm a mouse")
+local mouse = require("lovr-libraries.lovr-mouse")
+
+local posX = 0
+local posY = 0
+local oldPosX = 0
+local oldPosY = 0
+
+function PollMouse()
+    posX, posY = mouse.getPosition()
+end
+
+function GetMousePos()
+    return{posX, posY}
+end
