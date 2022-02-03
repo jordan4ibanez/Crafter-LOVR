a89382d3e33b66154efc595051571b99e895ba93
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-02 22:45:21 -0500

    Implement basic maths into chunkMeshBuilder

--- game/chunkMeshBuilder.lua
+++ game/chunkMeshBuilder.lua
@@ -1,34 +1,40 @@
+local xMax = 16;
+local yMax = 128;
+local ystride = xMax
+local zstride = xMax * yMax
+
+
+local function __index_to_pos(index)
+    index = index - 1
+    local z = math.floor( index / zstride)
+    index = index % zstride
+    local y = math.floor( index / ystride)
+    index = index % ystride
+    local x = math.floor(index)
+    -- Returns a Tuple
+    return x, y, z;
+end
 
 function BuildChunkMesh(chunk)
     local newChunkMesh
     -- This mesh is a single triangle
-    newChunkMesh = lovr.graphics.newMesh(
-        {
-            { 'lovrPosition', 'float', 3 },
-            { 'lovrNormal', 'float', 3 }
-        },
-        3,
-        'triangles'
-    )
-    local test = ""
+    newChunkMesh = lovr.graphics.newMesh({{"lovrPosition", "float", 3}, {"lovrNormal", "float", 3}}, 3, "fan", "static", false)
 
-    for i = 1,10000 do
-        test = test .. math.floor(math.random() * 10)
+    local meshVertex = {}
+
+    print("start " .. collectgarbage("count"))
+    for i = 1, #chunk.blocks do
+        local x,y,z = __index_to_pos(i)
+        --print("x: " .. x .. " y: " .. y .. " z: " .. z)
     end
+    print("end " .. collectgarbage("count"))
 
-    local blob = lovr.data.newBlob(test, "test")
 
-    test = nil
+    meshVertex[#meshVertex + 1] = {0,0,0, 0,0,1}
+    meshVertex[#meshVertex + 1] = {1,0,0, 0,0,1}
+    meshVertex[#meshVertex + 1] = {0,1,0, 0,0,1}
 
-    print(blob:getString())
-
-    newChunkMesh:setVertices(
-        {
-            {0,0,0, 0,0,1},
-            {1,0,0, 0,0,1},
-            {0,1,0, 0,0,1},
-        }
-    )
+    newChunkMesh:setVertices(meshVertex)
     
     return newChunkMesh
 end
