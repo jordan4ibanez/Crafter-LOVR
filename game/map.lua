7e2736b45e5a710f2d12872abd8e63d804763dd8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-02 22:50:03 -0500

    Turn on randomization for chunk block data

--- game/map.lua
+++ game/map.lua
@@ -59,7 +59,7 @@ local function __create_chunk(x, z, dimension)
         This is still 2D and needs to have more math backing a pure 1D implementation
     ]]--
     for i = 1, 32768 do
-        __chunk_blocks[index][i]    = 1
+        __chunk_blocks[index][i]    = math.floor(math.random() + 0.5)
         __chunk_rotations[index][i] = 1
         __chunk_light[index][i]     = 1
     end
