448722445398fbd8c4cdf5db078a67b9a65d1543
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-04 01:04:28 -0500

    Optimize FFI as much as possible within skill level

--- game/chunkMeshBuilder.lua
+++ game/chunkMeshBuilder.lua
@@ -1,10 +1,12 @@
 print("reloaded!--------")
+print("REMEMBER FFI ARRAYS START AT 0")
 
 -- lua locals
 local 
-floor
+floor, graphics, timer, data
 = 
-math.floor
+math.floor, lovr.graphics, lovr.timer, lovr.data
+
 local ffi = require('ffi')
 
 local __width = 16;
@@ -13,10 +15,10 @@ local __xCalc = __depth * __width
 local __zCalc = __depth
 
 -- Texture Atlas - Will be constructed of all mod textures in the future
-local texture = lovr.graphics.newTexture("textures/dirt.png", {mipmaps = true})
+local texture = graphics.newTexture("textures/dirt.png", {mipmaps = true})
 texture:setFilter("nearest")
 texture:setWrap("repeat", "repeat")
-local material = lovr.graphics.newMaterial(texture, 1, 1, 1, 1)
+local material = graphics.newMaterial(texture, 1, 1, 1, 1)
 
 -- Reconstructs 1D table point to 3D position
 local function __index_to_pos(index)
@@ -34,28 +36,6 @@ local function __index_to_pos(index)
     return x, y, z;
 end
 
--- Deconstructs 3D position to 1D table point
-local function __pos_to_index(x, y, z)
-    -- Returns an integer
-	return floor((z * __zCalc) + (x * __xCalc) + y + 1)
-end
-
--- Automates the indices injection
-local function __build_indices(indices, indicesCount, vertexCount)
-    -- Tri 1
-    indices[indicesCount + 1] = vertexCount + 1
-    indices[indicesCount + 2] = vertexCount + 2
-    indices[indicesCount + 3] = vertexCount + 4
-    -- Tri 2
-    indices[indicesCount + 4] = vertexCount + 4
-    indices[indicesCount + 5] = vertexCount + 2
-    indices[indicesCount + 6] = vertexCount + 3
-
-    -- Tick up counter
-    indicesCount = indicesCount + 6
-
-    return indices, indicesCount
-end
 
 --[[
 
@@ -71,161 +51,170 @@ end
 ]]--
 
 
-local function __face_up(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
-    -- NOTE: this was * x,  * y,  * z
-    vertices[vertexCount + 1] = {0,1,0, 0,1,0, 0,0}
-    vertices[vertexCount + 2] = {0,1,1, 0,1,0, 0,1}
-    vertices[vertexCount + 3] = {1,1,1, 0,1,0, 1,1}
-    vertices[vertexCount + 4] = {1,1,0, 0,1,0, 1,0}
-
-    indices, indicesCount = __build_indices(indices, indicesCount, vertexCount)
-
-    vertexCount = vertexCount + 4
-
-    return indices, indicesCount, vertexCount
+-- Deconstructs 3D position to 1D table point
+local function __pos_to_index(x, y, z)
+    -- Returns an integer
+	return floor((z * __zCalc) + (x * __xCalc) + y + 1)
 end
 
-local function __face_down(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
-    -- NOTE: this was * x,  * y,  * z
-    vertices[vertexCount + 1] = {0,0,1, 0,1,0, 0,1}
-    vertices[vertexCount + 2] = {0,0,0, 0,1,0, 0,0}
-    vertices[vertexCount + 3] = {1,0,0, 0,1,0, 1,0}
-    vertices[vertexCount + 4] = {1,0,1, 0,1,0, 1,1}
+local function __insert_data(x, y, z, w, q, array, arrayCount, xx, yy, zz)
 
-    indices, indicesCount = __build_indices(indices, indicesCount, vertexCount)
+    array[arrayCount    ] = x + xx
+    array[arrayCount + 1] = y + yy
+    array[arrayCount + 2] = z + zz
 
-    vertexCount = vertexCount + 4
+    array[arrayCount + 3] = w
+    array[arrayCount + 4] = q
 
-    return indices, indicesCount, vertexCount
+    arrayCount = arrayCount + 5
+
+    return array, arrayCount
 end
 
-local function __face_south(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+local function __face_up(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
     -- NOTE: this was * x,  * y,  * z
-    vertices[vertexCount + 1] = {0,1,1, 0,0,1, 0,1}
-    vertices[vertexCount + 2] = {0,0,1, 0,0,1, 0,0}
-    vertices[vertexCount + 3] = {1,0,1, 0,0,1, 1,0}
-    vertices[vertexCount + 4] = {1,1,1, 0,0,1, 1,1}
 
-    indices, indicesCount = __build_indices(indices, indicesCount, vertexCount)
+    -- Tri 1
+    array, arrayCount = __insert_data(0,1,0, 0,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,1,1, 0,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,1,1, 1,1, array, arrayCount, x, y, z)
 
-    vertexCount = vertexCount + 4
+    -- Tri 2
+    array, arrayCount = __insert_data(1,1,0, 1,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,1,0, 0,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,1,1, 1,1, array, arrayCount, x, y, z)
 
-    return indices, indicesCount, vertexCount
+    return array, arrayCount
 end
 
-local function __face_north(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+local function __face_down(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
     -- NOTE: this was * x,  * y,  * z
-    vertices[vertexCount + 1] = {0,0,0, 0,0,1, 0,0}
-    vertices[vertexCount + 2] = {0,1,0, 0,0,1, 0,1}
-    vertices[vertexCount + 3] = {1,1,0, 0,0,1, 1,1}
-    vertices[vertexCount + 4] = {1,0,0, 0,0,1, 1,0}
-    
 
-    indices, indicesCount = __build_indices(indices, indicesCount, vertexCount)
+    -- Tri 1
+    array, arrayCount = __insert_data(0,0,1, 0,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,0,0, 0,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,0,0, 1,0, array, arrayCount, x, y, z)
 
-    vertexCount = vertexCount + 4
+    -- Tri 2
+    array, arrayCount = __insert_data(1,0,1, 1,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,0,1, 0,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,0,0, 1,0, array, arrayCount, x, y, z)
 
-    return indices, indicesCount, vertexCount
+
+    return array, arrayCount
 end
 
-local function __face_west(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+
+local function __face_south(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
     -- NOTE: this was * x,  * y,  * z
-    vertices[vertexCount + 1] = {1,0,1, 1,0,0, 0,0}
-    vertices[vertexCount + 2] = {1,0,0, 1,0,0, 0,1}
-    vertices[vertexCount + 3] = {1,1,0, 1,0,0, 1,1}
-    vertices[vertexCount + 4] = {1,1,1, 1,0,0, 1,0}
 
-    indices, indicesCount = __build_indices(indices, indicesCount, vertexCount)
+    -- Tri 1
+    array, arrayCount = __insert_data(0,1,1, 0,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,0,1, 0,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,0,1, 1,0, array, arrayCount, x, y, z)
 
-    vertexCount = vertexCount + 4
+    -- Tri 2
+    array, arrayCount = __insert_data(1,1,1, 1,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,1,1, 0,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,0,1, 1,0, array, arrayCount, x, y, z)
 
-    return indices, indicesCount, vertexCount
+    return array, arrayCount
 end
 
-local function __face_east(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+
+local function __face_north(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
     -- NOTE: this was * x,  * y,  * z
-    vertices[vertexCount + 1] = {0,0,0, 1,0,0, 0,1}
-    vertices[vertexCount + 2] = {0,0,1, 1,0,0, 0,0}
-    vertices[vertexCount + 3] = {0,1,1, 1,0,0, 1,0}
-    vertices[vertexCount + 4] = {0,1,0, 1,0,0, 1,1}
+    -- Tri 1
+    array, arrayCount = __insert_data(0,0,0, 0,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,1,0, 0,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,1,0, 1,1, array, arrayCount, x, y, z)
 
-    indices, indicesCount = __build_indices(indices, indicesCount, vertexCount)
+    -- Tri 2
+    array, arrayCount = __insert_data(1,0,0, 1,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,0,0, 0,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,1,0, 1,1, array, arrayCount, x, y, z)
 
-    vertexCount = vertexCount + 4
-
-    return indices, indicesCount, vertexCount
+    return array, arrayCount
 end
+
+
+local function __face_west(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
+    -- NOTE: this was * x,  * y,  * z
+
+    -- Tri 1
+    array, arrayCount = __insert_data(1,0,1, 0,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,0,0, 0,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,1,0, 1,1, array, arrayCount, x, y, z)
+
+    -- Tri 2
+    array, arrayCount = __insert_data(1,1,1, 1,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,0,1, 0,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(1,1,0, 1,1, array, arrayCount, x, y, z)
+
+    return array, arrayCount
+end
+
+local function __face_east(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
+    -- NOTE: this was * x,  * y,  * z
+    array, arrayCount = __insert_data(0,0,0, 0,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,0,1, 0,0, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,1,1, 1,0, array, arrayCount, x, y, z)
+
+    array, arrayCount = __insert_data(0,1,0, 1,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,0,0, 0,1, array, arrayCount, x, y, z)
+    array, arrayCount = __insert_data(0,1,1, 1,0, array, arrayCount, x, y, z)
+
+    return array, arrayCount
+end
+
+
 
 -- Builds a single block of the chunk's mesh - utilizes localized functions for this
-local function __buildCube(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+local function __buildCube(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
 
-    indices, indicesCount, vertexCount = __face_up(array,x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+    array, arrayCount =    __face_up(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
 
-    indices, indicesCount, vertexCount = __face_down(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+    array, arrayCount =  __face_down(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
 
-    indices, indicesCount, vertexCount = __face_north(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+    array, arrayCount = __face_north(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
 
-    indices, indicesCount, vertexCount = __face_south(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+    array, arrayCount = __face_south(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
 
-    indices, indicesCount, vertexCount = __face_west(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+    array, arrayCount = __face_west(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
 
-    indices, indicesCount, vertexCount = __face_east(array, x, y, z, width, height, texturePointX, texturePointY, vertices, vertexCount, indices, indicesCount)
+    array, arrayCount = __face_east(array, arrayCount, x, y, z, width, height, texturePointX, texturePointY)
 
-    return vertexCount, indices, indicesCount
+    return array, arrayCount
 end
 
 
 function BuildChunkMesh(chunk)
-    local newChunkMesh
 
-    local vertices = {}
-    local vertexCount = 0
+    local arrayCounter = 0
 
-    local indices = {}
-    local indicesCount = 0
-
-    local start = lovr.timer.getTime()
-
+    local start = timer.getTime()
+    
 
     local prescan = 0
 
-    local blocksPointer = chunk.blocks
+    -- local blocksPointer = chunk.blocks
 
+    --[[
     for i = 1,32768 do
         if blocksPointer[i] > 0 then
             prescan = prescan + 1
         end
     end
+    ]]
 
-    -- prescan * bits * indexes per face * faces
-    local myBlob = lovr.data.newBlob(15 * 8, "blobby")
+    -- prescan blocks * bits * (vertex + textureMap + normals)
+
+    local myBlob = data.newBlob(32768 * 8 * (30 * 6), "blobby")
     local array = ffi.cast("float*", myBlob:getPointer())
 
 
-    array[0] = 0.0
-    array[1] = 0.0
-    array[2] = 0.0
-
-    array[3] = 0
-    array[4] = 0
-
-    array[5] = 1.0
-    array[6] = 0.0
-    array[7] = 0.0
-
-    array[8] = 1
-    array[9] = 0
-    
-    array[10] = 0.0
-    array[11] = 1.0
-    array[12] = 0.0
-
-    array[13] = 0
-    array[14] = 1
-
-    -- print("----------STARTING TEST------------")
-    -- print("start " .. collectgarbage("count"))
-    --[[
+    print("----------STARTING TEST------------")
+    print("start " .. collectgarbage("count"))
     for i = 1,32768 do
         local x,y,z = __index_to_pos(i)
         local index = __pos_to_index(x,y,z)
@@ -236,24 +225,29 @@ function BuildChunkMesh(chunk)
             -- print("FAILURE AT: " .. i .. " IS EQUAL TO " .. index .. "! MATH MISCALCULATION!")
         -- end
         -- print("x: " .. x .. " y: " .. y .. " z: " .. z)
-        -- vertexCount, indices, indicesCount = __buildCube(array, x,y,z,nil,nil,nil,nil,vertices, vertexCount, indices, indicesCount)
+        array,arrayCounter = __buildCube(array, arrayCounter, x,y,z,nil,nil,nil,nil)
 
         x,y,z,index = nil,nil,nil,nil
     end
-    ]]
 
     -- This mesh is a single triangle
-    newChunkMesh = lovr.graphics.newMesh({{"lovrPosition", "float", 3}, {"lovrTexCoord", "float", 2}}, myBlob, "triangles", "static", false)
+    local newChunkMesh = graphics.newMesh(
+        {
+            {"lovrPosition", "float", 3}, {"lovrTexCoord", "float", 2}
+        },
+        myBlob, "triangles", "static", false)
 
+    --release memory
+    -- array = nil
     -- myBlob:release()
-    -- newChunkMesh:setVertices(vertices)
 
-    -- newChunkMesh:setVertexMap(indices)
+    --array = nil
+    --myBlob = nil
 
     newChunkMesh:setMaterial(material)
     
-    -- print("end " .. collectgarbage("count"))
-    -- print("Elapsed time: " .. lovr.timer.getTime() - start)
-    -- print("------------TEST HAS ENDED----------------")
+    print("end " .. collectgarbage("count"))
+    print("Elapsed time: " .. timer.getTime() - start)
+    print("------------TEST HAS ENDED----------------")
     return newChunkMesh
 end
